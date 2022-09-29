d525d7cfe0a7abec1861250bbc2fe7b620493ff3
milena <miladar050813@gmail.com>
2022-09-29 13:08:09 +0300

    Переменные 1-3 задание

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        double dog = 8;
+        dog = dog + 4;
+        dog = dog - 3.5;
+        System.out.println(dog);
+
+
+        var cat = 3.6;
+        cat = cat + 4;
+        cat = cat - 1.6;
+        System.out.println(cat);
+
+        var paper = 763789;
+        paper = paper + 4;
+        paper = paper - 7639;
+        System.out.println(paper);
+    }
+}
