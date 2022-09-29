bfd43538123bc0a7849d1cbe5ed1b5710cf89d38
milena <miladar050813@gmail.com>
2022-09-29 13:47:08 +0300

    Переменные 4-5 задания добавлены

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,45 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Первое задание");
         double dog = 8;
+        System.out.println(dog);
         dog = dog + 4;
+        System.out.println(dog);
         dog = dog - 3.5;
         System.out.println(dog);
 
-
+        System.out.println("Второе задание");
         var cat = 3.6;
+        System.out.println(cat);
         cat = cat + 4;
+        System.out.println(cat);
         cat = cat - 1.6;
         System.out.println(cat);
 
+        System.out.println("Третье задание");
         var paper = 763789;
+        System.out.println(paper);
         paper = paper + 4;
+        System.out.println(paper);
         paper = paper - 7639;
         System.out.println(paper);
+
+        System.out.println("Четвертое задание");
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+
+        System.out.println("Пятое задание");
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
     }
 }
